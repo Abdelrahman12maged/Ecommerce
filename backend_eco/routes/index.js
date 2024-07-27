ecc0604e437c554c6f86b93365b357fef6e87fc0
Abdelrahman12maged <abdomaged01206@gamil.com>
2024-07-28 00:14:45 +0300

    lll

---
+++ backend_eco/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const router = express.Router()
+const userSignUpController = require("../controllers/user/signup")
+
+
+router.post("/signup",userSignUpController)
+
+
+
+
+
+
+
+
+
+module.exports = router
